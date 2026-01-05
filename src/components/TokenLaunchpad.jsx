429af854f1471a6f08f77bf28954dd6fbb4d2fb2
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-01-05 20:14:28 +0530

    Wallet Adapter

--- src/components/TokenLaunchpad.jsx
+++ src/components/TokenLaunchpad.jsx
@@ -1,4 +1,30 @@
+import { createInitializeMint2Instruction, getMinimumBalanceForRentExemptMint } from "@solana/spl-token";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+
 export function TokenLaunchpad() {
+    const wallet = useWallet();
+    async function createToken() {
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+        const keypair = Keypair.generate();
+
+        // You create a new mint account
+        // You first create a keypair for this new mint account
+        // owners
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: payer.publicKey,
+                newAccountPubkey: keypair.publicKey,
+                space: MINT_SIZE,
+                lamports,
+                programId,   // who owns this account
+            }),
+            createInitializeMint2Instruction(keypair.publicKey, decimals, mintAuthority, freezeAuthority, programId),
+        );
+        
+        transaction.partialSign(keypair);
+        await wallet.signTransaction(transaction);
+    }
     return <div style={{
         height: '100vh',
         display: 'flex',
@@ -11,6 +37,6 @@ export function TokenLaunchpad() {
         <input className='inputText' type="text" placeholder="Symbol"></input> <br />
         <input className='inputText' type="text" placeholder="Image URL"></input> <br />
         <input className='inputText' type="text" placeholder="Initial Supply"></input> <br />
-        <button className='btn'>Create a Token</button>
+        <button onClick={createToken} className='btn'>Create a Token</button>
     </div>;
 }
