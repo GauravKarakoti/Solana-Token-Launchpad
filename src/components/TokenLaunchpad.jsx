7458efcccbe60df71ed89bace656284f9e925af2
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-01-06 22:49:14 +0530

    Without Metadata

--- src/components/TokenLaunchpad.jsx
+++ src/components/TokenLaunchpad.jsx
@@ -1,10 +1,15 @@
-import { createInitializeMint2Instruction, getMinimumBalanceForRentExemptMint } from "@solana/spl-token";
-import { useWallet } from "@solana/wallet-adapter-react";
+import { createInitializeMint2Instruction, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
 
 export default function TokenLaunchpad() {
     const wallet = useWallet();
+    const { connection } = useConnection();
     async function createToken() {
+        const name =document.getElementById("name").value;
+        const symbol =document.getElementById("symbol").value;
+        const imageUrl =document.getElementById("imageUrl").value;
+        const initialSupply =document.getElementById("initialSupply").value;
         const lamports = await getMinimumBalanceForRentExemptMint(connection);
         const keypair = Keypair.generate();
 
@@ -13,30 +18,34 @@ export default function TokenLaunchpad() {
         // owners
         const transaction = new Transaction().add(
             SystemProgram.createAccount({
-                fromPubkey: payer.publicKey,
+                fromPubkey: wallet.publicKey,
                 newAccountPubkey: keypair.publicKey,
                 space: MINT_SIZE,
                 lamports,
-                programId,   // who owns this account
+                programId: TOKEN_PROGRAM_ID,   // who owns this account
             }),
-            createInitializeMint2Instruction(keypair.publicKey, decimals, mintAuthority, freezeAuthority, programId),
+            createInitializeMint2Instruction(keypair.publicKey, 6, wallet.publicKey, wallet.publicKey, TOKEN_PROGRAM_ID),
         );
+        const recentBlockhash = await connection.getLatestBlockhash();
+        transaction.recentBlockhash = recentBlockhash.blockhash;
+        transaction.feePayer = wallet.publicKey;
         
         transaction.partialSign(keypair);
-        await wallet.signTransaction(transaction);
+        let response = await wallet.sendTransaction(transaction, connection);
+        console.log(response);
     }
     return <div style={{
-        height: '80vh',
+        height: '60vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
     }} className="launchpad">
         <h1 className="title">Solana Token Launchpad</h1>
-        <input className='inputText' type="text" placeholder="Name"></input> <br />
-        <input className='inputText' type="text" placeholder="Symbol"></input> <br />
-        <input className='inputText' type="text" placeholder="Image URL"></input> <br />
-        <input className='inputText' type="text" placeholder="Initial Supply"></input> <br />
+        <input id="name" className='inputText' type="text" placeholder="Name"></input> <br />
+        <input id="symbol" className='inputText' type="text" placeholder="Symbol"></input> <br />
+        <input id="imageUrl" className='inputText' type="text" placeholder="Image URL"></input> <br />
+        <input id="initialSupply" className='inputText' type="text" placeholder="Initial Supply"></input> <br />
         <button onClick={createToken} className='btn'>Create a Token</button>
     </div>;
 }
